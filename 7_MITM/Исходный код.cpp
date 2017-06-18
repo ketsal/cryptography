59249cc30777ee0771664faecf9bec261bf4473d
ketsal <dmitriev.step@gmail.com>
2017-06-18 19:46:56 +0500

    donemitm

--- 7_MITM/Исходный код.cpp
+++ 7_MITM/Исходный код.cpp
@@ -7,6 +7,111 @@
 #include "../include/DESProxy.h"
 #include "../include/file.h"
 #include <map>
+#include <thread>
+#include <mutex>
+std::mutex locker;
+void ResultOfEncryption(std::map <  std::vector<unsigned char>, std::string > &MAP, unsigned char **KEYS, std::vector<unsigned char> data, int start, int end, int DecOrEnc)
+{
+    DesProxy DoubleDES;
+    std::string KEY = "1234567";
+    for (int i = start; i < end; i++)
+    {
+        DoubleDES.GetEncData().clear();
+        DoubleDES.GetDecData().clear();
+        locker.lock();
+        if (DecOrEnc == 1)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                KEY[j] = KEYS[i][j];
+            }
+        }
+        else
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                KEY[j] = KEYS[i][j + 7];
+            }
+        }
+        locker.unlock();
+        const char* charsKEY = KEY.data();
+        const  char* KEYbytes = reinterpret_cast<const char*>(charsKEY);
+        if (DecOrEnc==1)
+        {
+            DoubleDES.EncryptCFB(data, KEYbytes);
+        }
+        else
+        {
+            DoubleDES.DecryptCFB(data, KEYbytes);
+        }
+        std::vector<unsigned char> temp;
+        if (DecOrEnc == 1)
+        {
+            temp = DoubleDES.GetEncData();
+        }
+        else
+        {
+            temp = DoubleDES.GetDecData();
+        }
+        MAP.insert(std::pair<std::vector<unsigned char>,std::string>
+            (temp, KEY));
+        temp.clear();
+    }
+}
+void CreateKeys(unsigned char **Keys, int numberofkeys)
+{
+    int j = 13;
+    bool cont = true;
+    for (int i = 0; i < 14; i++)
+    {
+        Keys[0][i] = 0 + rand() % 255;
+    }
+    for (int i = 1; i < numberofkeys; i++)
+    {
+        cont = true;
+        j = 13;
+        for (int k = 0; k < 14; k++)
+        {
+            Keys[i][k] = Keys[i - 1][k];
+        }
+        Keys[i][j] = Keys[i - 1][j] + 1;
+        if (Keys[i][j] == 0)
+        {
+            while (Keys[i][j] == 0)
+            {
+                j--;
+                Keys[i][j] = Keys[i - 1][j] + 1;
+            }
+        }
+    }
+}
+bool FindPairs(
+    std::map <  std::vector<unsigned char>, std::string > &ENCMAP
+    , std::map <  std::vector<unsigned char>, std::string > &DECMAP1
+    , std::map <  std::vector<unsigned char>, std::string > &DECMAP2
+    , std::map <  std::vector<unsigned char>, std::string > &DECMAP3
+    , std::map <  std::vector<unsigned char>, std::string > &DECMAP4
+    , std::vector<unsigned char> &Data
+    , std::vector<unsigned char> &Key)
+{
+    bool found = false;
+    for (auto it = ENCMAP.begin(); it != ENCMAP.end(); it++)
+    {
+        if ((DECMAP1.count(it->first) > 0) || (DECMAP2.count(it->first) > 0) || (DECMAP3.count(it->first) > 0) || (DECMAP4.count(it->first) > 0))
+        {
+            Data = it->first;
+            for (int j = 0; j < 7; j++)
+            {
+                if (j < 7)
+                {
+                    Key.push_back(it->second[j]);
+                }
+            }
+            found = true;
+        }
+    }
+    return found;
+}
 void main()
 {
     DesProxy DoubleDES;
@@ -15,43 +120,17 @@ void main()
     File plain(path);
     File crypted(path2);
     srand(time(NULL));
-    int numberofkeys = 10;
+    int numberofkeys = 65536;
+    bool cont = true;
+    bool found = false;
+    int KeyNumber = rand() % numberofkeys + 0;
     unsigned char **KEYS = new  unsigned char*[numberofkeys];
     for (int count = 0; count < numberofkeys; count++)
         KEYS[count] = new unsigned char[14];
-    for (int i = 0; i < 14; i++)
-    {
-        KEYS[0][i] = 0 + rand() % 255;
-    }
     plain.Read();
-    bool cont = true;
-    int j = 13;
-    KEYS[0][13] = 255;
-    for (int i = 1; i < numberofkeys; i++)
-    {
-        cont = true;
-        j = 13;
-        for (int k = 0; k < 14; k++)
-        {
-            KEYS[i][k] = KEYS[i - 1][k];
-        }
-        KEYS[i][j] = KEYS[i - 1][j] + 1;
-        if (KEYS[i][j] == 0)
-        {
-            j--;
-            KEYS[i][j] = KEYS[i - 1][j] + 1;
-            if (KEYS[i][j] == 0)
-            {
-                j--;
-                KEYS[i][j] = KEYS[i - 1][j] + 1;
-            }
-        }
-    }
-    int KeyNumber = rand() % 9 + 0;
-    unsigned char KEY[14];
+    CreateKeys(KEYS, numberofkeys);
     std::string KEY1 = "1234567";
     std::string KEY2 = "1234567";;
-
     for (int i = 0; i < 7; i++)
     {
         KEY1[i] = KEYS[KeyNumber][i];
@@ -60,89 +139,100 @@ void main()
     {
         KEY2[i] = KEYS[KeyNumber][i + 7];
     }
-    const char* chars = KEY1.data();
-    const  char* data_bytes = reinterpret_cast<const char*>(chars);
-    const char* chars2 = KEY2.data();
-    const  char* data_bytes2 = reinterpret_cast<const char*>(chars2);
-
-    DoubleDES.EncryptECB(plain.GetData(), data_bytes);
+    const char* KEY1chars = KEY1.data();
+    const  char* KEY1bytes = reinterpret_cast<const char*>(KEY1chars);
+    const char* KEY2chars = KEY2.data();
+    const  char* KEY2bytes = reinterpret_cast<const char*>(KEY2chars);
+    DoubleDES.EncryptCFB(plain.GetData(), KEY1bytes);
     std::vector<unsigned char> temp;
     temp = DoubleDES.GetEncData();
     DoubleDES.GetEncData().clear();
-    DoubleDES.EncryptECB(temp, data_bytes2);
+    DoubleDES.EncryptCFB(temp, KEY2bytes);
     crypted.GetData() = DoubleDES.GetEncData();
     crypted.WriteData();
     DoubleDES.GetEncData().clear();
-    DoubleDES.DecryptECB(crypted.GetData(), data_bytes2);
-    DoubleDES.EncryptECB(plain.GetData(), data_bytes);
-    std::map < unsigned char *, std::string > ENCMAP;
-    std::map < unsigned char *, std::string  > DECMAP;
-    for (int i = 0; i < numberofkeys; i++)
+    temp.clear();
+    std::map <  std::vector<unsigned char>, std::string > ENCMAP1;
+    std::map <  std::vector<unsigned char>, std::string  > DECMAP1;
+    std::map <  std::vector<unsigned char>, std::string > ENCMAP2;
+    std::map <  std::vector<unsigned char>, std::string  > DECMAP2;
+    std::map <  std::vector<unsigned char>, std::string > ENCMAP3;
+    std::map <  std::vector<unsigned char>, std::string  > DECMAP3;
+    std::map <  std::vector<unsigned char>, std::string > ENCMAP4;
+    std::map <  std::vector<unsigned char>, std::string  > DECMAP4;
+    std::thread Thread1(ResultOfEncryption, std::ref(ENCMAP1), KEYS, plain.GetData(), 0, 16384, 1);
+    std::thread Thread2(ResultOfEncryption, std::ref(ENCMAP2), KEYS, plain.GetData(), 16384, 32768, 1);
+    std::thread Thread3(ResultOfEncryption, std::ref(ENCMAP3), KEYS, plain.GetData(), 32768, 49152, 1);
+    std::thread Thread4(ResultOfEncryption, std::ref(ENCMAP4), KEYS, plain.GetData(), 49152, numberofkeys, 1);
+    std::thread Thread5(ResultOfEncryption, std::ref(DECMAP1), KEYS, crypted.GetData(), 0, 16384, 0);
+    std::thread Thread6(ResultOfEncryption, std::ref(DECMAP2), KEYS, crypted.GetData(), 16384, 32768, 0);
+    std::thread Thread7(ResultOfEncryption, std::ref(DECMAP3), KEYS, crypted.GetData(), 32768, 49152, 0);
+    std::thread Thread8(ResultOfEncryption, std::ref(DECMAP4), KEYS, crypted.GetData(), 49152, numberofkeys, 0);
+    std::cout << "Forming tables...\n";
+    Thread1.join();
+    Thread2.join();
+    Thread3.join();
+    Thread4.join();
+    Thread5.join();
+    Thread6.join();
+    Thread7.join();
+    Thread8.join();
+    system("cls");
+    std::cout << "Searching for pairs...\n";
+    std::vector<unsigned char> Data;
+    std::vector<unsigned char> Key;
+    found=FindPairs(ENCMAP1, DECMAP1, DECMAP2, DECMAP3, DECMAP4, Data, Key);
+    if (!found)
     {
-        DoubleDES.GetEncData().clear();
-        for (int j = 0; j < 7; j++)
-        {
-            KEY1[j] = KEYS[i][j];
-        }
-        const char* chars3 = KEY1.data();
-        const  char* data_bytes3 = reinterpret_cast<const char*>(chars3);
-        DoubleDES.EncryptECB(plain.GetData(), data_bytes3);
-        int  size = DoubleDES.GetEncData().size();
-        unsigned char *temp=new unsigned char[size];
-        for (int k = 0; k < size; k++)
-        {
-            temp[k] = DoubleDES.GetEncData().at(k);
-        }
-        ENCMAP.insert(std::pair <unsigned char*, std::string >
-            (temp, KEY1));
+        found = FindPairs(ENCMAP2, DECMAP1, DECMAP2, DECMAP3, DECMAP4, Data, Key);
     }
-    for (int i = 0; i < numberofkeys; i++)
+    if (!found)
     {
-        DoubleDES.GetDecData().clear();
-        for (int j = 0; j < 7; j++)
-        {
-            KEY2[j] = KEYS[i][j+7];
-        }
-        const char* chars3 = KEY2.data();
-        const  char* data_bytes3 = reinterpret_cast<const char*>(chars3);
-        DoubleDES.DecryptECB(crypted.GetData(), data_bytes3);
-        int size = DoubleDES.GetDecData().size();
-        unsigned char * temp=new unsigned char[size];
-        for (int k = 0; k < size; k++)
-        {
-            temp[k] = DoubleDES.GetDecData().at(k);
-        }
-        DECMAP.insert(std::pair<unsigned char*, std::string >
-            (temp,KEY2));
+        found = FindPairs(ENCMAP3, DECMAP1, DECMAP2, DECMAP3, DECMAP4, Data, Key);
     }
-    std::map <unsigned char*, std::string  > INTER;
-    std::set_intersection(ENCMAP.begin(), ENCMAP.end()
-        , DECMAP.begin(), DECMAP.end(), 
-        inserter(INTER,INTER.end()));
-    int size;
+    if (!found)
+    {
+        found = FindPairs(ENCMAP4, DECMAP1, DECMAP2, DECMAP3, DECMAP4, Data, Key);
+    }
+    int a = 0;
     cont = true;
-    bool found = true;
-    std::vector<unsigned char> tempvec;
-    std::vector<unsigned char> tempvec2;
-    std::vector<unsigned char> key;
-    for (auto itMap = DECMAP.begin(), it = ENCMAP.begin(); ((itMap != DECMAP.end()) && cont); it++, itMap++)
+    if ((DECMAP1.count(Data) > 0)&&cont)
     {
-        size = sizeof(itMap->first);
-        for (int j = 0; j < size; j++)
-        {
-            tempvec.push_back(itMap->first[j]);
-            tempvec2.push_back(it->first[j]);
-        }
-        if (tempvec == tempvec2)
-        {
-            cont = false;
-            key.insert(key.begin(),it->second[7]);
-            key.insert(key.end(), itMap->second[7]);
-            std::cout << "true";
-        }
+        a = 1;
+        cont = false;
     }
-    printf("%s\n", KEYS[KeyNumber]);
-    for (int j = 0; j < key.size(); j++)
-        std::cout << key.at(j);
+    if ((DECMAP2.count(Data) > 0) && cont)
+    {
+        a = 2;
+        cont = false;
+    }
+    if ((DECMAP3.count(Data) >0) && cont)
+    {
+        a = 3;
+        cont = false;
+    }
+    if ((DECMAP4.count(Data) > 0) && cont)
+    {
+        a = 4;
+        cont = false;
+    }
+    for (int j = 0; j < 7; j++)
+    {
+        if(a==1)
+            Key.push_back(DECMAP1.at(Data)[j]);
+        if (a == 2)
+            Key.push_back(DECMAP2.at(Data)[j]);
+        if (a == 3)
+            Key.push_back(DECMAP3.at(Data)[j]);
+        if (a == 4)
+            Key.push_back(DECMAP4.at(Data)[j]);
+    }
+    system("cls");
+    printf("Found the key\n");
+    for (int j = 0; j < Key.size(); j++)
+        printf("%X", Key.at(j));
+    printf("\nThe key used for encryption\n");
+    for (int j = 0; j <14; j++)
+        printf("%X",KEYS[KeyNumber][j]);
     system("pause");
 }
